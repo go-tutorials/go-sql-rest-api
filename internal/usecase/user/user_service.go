bc233f5273751f291dd3816154bea9127214d136
Duc Nguyen <minhduc140583@gmail.com>
2022-05-29 23:36:07 +0700

    Apply core-go/sql and core-go/service

--- internal/usecase/user/user_service.go
+++ internal/usecase/user/user_service.go
@@ -1,6 +1,11 @@
 package user
 
-import "context"
+import (
+	"context"
+	"database/sql"
+	sv "github.com/core-go/service"
+	q "github.com/core-go/sql"
+)
 
 type UserService interface {
 	Load(ctx context.Context, id string) (*User, error)
@@ -10,22 +15,40 @@ type UserService interface {
 	Delete(ctx context.Context, id string) (int64, error)
 }
 
-func NewUserService(repository UserRepository) UserService {
-	return &userService{repository: repository}
+func NewUserService(db *sql.DB, repository sv.Repository) UserService {
+	return &userService{db: db, repository: repository}
 }
 
 type userService struct {
-	repository UserRepository
+	db         *sql.DB
+	repository sv.Repository
 }
 
 func (s *userService) Load(ctx context.Context, id string) (*User, error) {
-	return s.repository.Load(ctx, id)
+	var user User
+	ok, err := s.repository.LoadAndDecode(ctx, id, &user)
+	if !ok {
+		return nil, err
+	} else {
+		return &user, err
+	}
 }
 func (s *userService) Create(ctx context.Context, user *User) (int64, error) {
-	return s.repository.Create(ctx, user)
+	ctx, tx, err := q.Begin(ctx, s.db)
+	if err != nil {
+		return  -1, err
+	}
+	res, err := s.repository.Insert(ctx, user)
+	return q.End(tx, res, err)
 }
 func (s *userService) Update(ctx context.Context, user *User) (int64, error) {
-	return s.repository.Update(ctx, user)
+	ctx, tx, err := q.Begin(ctx, s.db)
+	if err != nil {
+		return  -1, err
+	}
+	res, err := s.repository.Update(ctx, user)
+	err = q.Commit(tx, err)
+	return res, err
 }
 func (s *userService) Patch(ctx context.Context, user map[string]interface{}) (int64, error) {
 	return s.repository.Patch(ctx, user)
